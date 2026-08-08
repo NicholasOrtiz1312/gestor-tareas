2b1b85f9c82f76228de9f1ee7a1b32400ed9b216
NicholasOrtiz1312 <nortiz284@puce.edu.ec>
2026-08-07 19:53:24 -0500

    Cambio

--- main.py
+++ main.py
@@ -6,3 +6,10 @@ def mostrar_menu():
     print("2. Listar tareas")
     print("3. Mostrar progreso")
     print("4. Salir")
+
+def listar_tareas():
+     for tarea in tareas:
+        print(
+            tarea["nombre"],
+            tarea["completada"]
+        )
